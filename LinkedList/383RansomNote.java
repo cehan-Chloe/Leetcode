f3e8fffa77b230864c9058ee257a8d66d7d94fca
Chloe <cehan16@gmail.com>
2016-10-13 11:00:59 -0400

    Q383

---
+++ LinkedList/383RansomNote.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        for (int i = 0; i < magazine.length(); i++){
+            arr[magazine.charAt(i) - 'a'] ++;
+        }
+        for (int j = 0; j < ransomNote.length(); j++){
+            if (--arr[ransomNote.charAt(j) - 'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
